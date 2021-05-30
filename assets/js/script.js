4582003627e5f1ff210d617085aa68d35b280f73
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-05-30 18:01:39 -0400

    display api results

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,54 +2,59 @@ $(".fc").hide()
 var button = document.querySelector("#searchBtn");
 
 function getCurrentWeather() {
-    var city = document.querySelector("#city").value;
-    var apiKey = '18bb77d524842388b83512049fe6a263';
-    var currentUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
-    if (city) {
-        fetch(currentUrl)
-            .then(data => data.json())
-            .then(res => {
-                var weather = res;
-                var name = document.getElementById("name");
-                var date = moment().format('MM/DD/YYYY');
-                var iconId = weather.weather[0].icon;
-                var icon = '<img src="http://openweathermap.org/img/w/' + iconId + '.png" />'
-                name.innerHTML = weather.name + " " + "(" + date + ")" + icon;
-                var tempC = weather.main.temp - 273.15;
-                tempC = tempC.toFixed(0);
-                var temp = document.getElementById("temp");
-                temp.textContent = "Temp: " + tempC + "°C";
-                var wind = document.getElementById("wind");
-                wind.textContent = "Wind: " + weather.wind.speed + " MPH";
-                var humidity = document.getElementById("humidity");
-                humidity.textContent = "Humidity: " + weather.main.humidity + "%";
-                var lat = weather.coord.lat;
-                var lon = weather.coord.lon;
-                fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily&appid=' + apiKey)
-                    .then(data => data.json())
-                    .then(res => {
-                        console.log(res);
-                        var uvi = Number(res.current.uvi);
-                        var uvElement = document.createElement("p");
-                        uvElement.textContent = uvi;
-                        uvElement.style.display = "inline-block";
-                        var uvIndex = document.getElementById('uv');
-                        uvIndex.innerHTML = "UV Index: ";
-                        if (uvi <= 2) {
-                            uvElement.style.backgroundColor = "green";
-                        }
-                        else if (uvi > 2 && uvi <= 5) {
-                            uvElement.style.backgroundColor = "yellow";
-                        }
-                        else if (uvi > 5) {
-                            uvElement.style.backgroundColor = "red";
-                        }
-                        uvIndex.appendChild(uvElement);
-                    })
+    try {
+        var city = document.querySelector("#city").value;
+        var apiKey = '18bb77d524842388b83512049fe6a263';
+        var currentUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+        if (city) {
+            fetch(currentUrl)
+                .then(data => data.json())
+                .then(res => {
+                    var weather = res;
+                    var name = document.getElementById("name");
+                    var date = moment().format('MM/DD/YYYY');
+                    var iconId = weather.weather[0].icon;
+                    var icon = '<img src="http://openweathermap.org/img/w/' + iconId + '.png" />'
+                    name.innerHTML = weather.name + " " + "(" + date + ")" + icon;
+                    var tempC = weather.main.temp - 273.15;
+                    tempC = tempC.toFixed(0);
+                    var temp = document.getElementById("temp");
+                    temp.textContent = "Temp: " + tempC + "°C";
+                    var wind = document.getElementById("wind");
+                    wind.textContent = "Wind: " + weather.wind.speed + " MPH";
+                    var humidity = document.getElementById("humidity");
+                    humidity.textContent = "Humidity: " + weather.main.humidity + "%";
+                    var lat = weather.coord.lat;
+                    var lon = weather.coord.lon;
+                    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily&appid=' + apiKey)
+                        .then(data => data.json())
+                        .then(res => {
+                            console.log(res);
+                            var uvi = Number(res.current.uvi);
+                            var uvElement = document.createElement("p");
+                            uvElement.textContent = uvi;
+                            uvElement.style.display = "inline-block";
+                            var uvIndex = document.getElementById('uv');
+                            uvIndex.innerHTML = "UV Index: ";
+                            if (uvi <= 2) {
+                                uvElement.style.backgroundColor = "green";
+                            }
+                            else if (uvi > 2 && uvi <= 5) {
+                                uvElement.style.backgroundColor = "yellow";
+                            }
+                            else if (uvi > 5) {
+                                uvElement.style.backgroundColor = "red";
+                            }
+                            uvIndex.appendChild(uvElement);
+                        })
 
-            })
+                })
 
 
+        }
+    }
+    catch (err) {
+        console.log(err);
     }
 }
 
@@ -111,8 +116,11 @@ function getForecast() {
     }
 }
 
+
 button.addEventListener("click", function () {
     getCurrentWeather();
     getForecast();
 })
+
+
 
